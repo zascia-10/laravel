a16c78e4e7be1cb1e5120a8c455a75c37428cbfb
GEVTA ARISANDI <gevta.arisandi45@smk.belajar.id.com>
2025-10-17 12:44:15 +0700

    tambah file products

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -19,7 +19,7 @@
                 </div>
                 <div class="card border-0 shadow-sm rounded">
                     <div class="card-body">
-                    
+                        <a href="{{ route('products.create') }}" class="btn btn-md btn-success mb-3">ADD PRODUCT</a>
                         <table class="table table-bordered">
                             <thead>
                                 <tr>
@@ -56,7 +56,7 @@
                                 @endforelse
                             </tbody>
                         </table>
-                    
+                        {{ $products->links() }}
                     </div>
                 </div>
             </div>
--- routes/web.php
+++ routes/web.php
@@ -7,4 +7,8 @@ use App\Http\Controllers\ProductController;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/product',[ProductController::class,'index']);
+//Route:: get('/products', [ProductsController::class, 'index'])->name('products.index');
+//Route:: get('/products', [ProductsController::class, 'store'])->name('products.store');
+//Route:: get('/products/create', [ProductsController::class, 'create'])->name('products.create');
+//Route:: get('/products/{id}', [ProductsController::class, 'destoy'])->name('products.destoy');
+Route::resource('/products',ProductController::class,);
